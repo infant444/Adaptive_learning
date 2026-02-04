5be9f698614fe56396aaba04942e1fca709ad9cf
infant04 <infant0467@gmail.com>
2026-02-04 11:39:01 +0530

    Init a frontend and backend

---
+++ backend/src/server.ts
@@ -0,0 +1,33 @@
+import dorenv from 'dotenv';
+dorenv.config();
+import express from 'express';
+import cors from 'cors';
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
+import cookieParser from "cookie-parser";
+import { errorHandler } from './middleware/error.middleware';
+
+const app=express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    credentials:true,
+    origin:["http://localhost:3000"]
+}));
+
+// Attach WebSocket server
+
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        message:"Hello Welcome to Adaptive Learning" 
+    })
+})
+
+app.use(errorHandler);
+
+
+const port=process.env.PORT || 5000;
+app.listen(port,()=>{
+    console.log("serve on http://localhost:"+port);
+    
+})
