de465796046444a32406a32ba9b05ced463dacfa
puviyarasans <puviyarasansasi123@gmail.com>
2026-03-09 17:00:44 +0530

    push

--- backend/src/config/mail.config.ts
+++ backend/src/config/mail.config.ts
@@ -5,6 +5,9 @@ export const MailConfig={
     auth:{
         user:process.env.EMAIL_USER!,
         pass:process.env.EMAIL_PASSWORD!,
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 }
 export const mailGenerator = new Mailgen({
